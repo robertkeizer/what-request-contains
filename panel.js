33acbbad7c734456e1845869acfa33b1bfa2231a
Robert Keizer <robert@keizer.ca>
2016-08-28 16:54:29 -0500

    Basic getting of the network requests.

--- panel.js
+++ panel.js
@@ -1,5 +1,18 @@
-console.log( "HI from panel" );
+$(document).ready( function( ){
+	function updatePanel( ){
 
-setTimeout( function( ){
-	alert( "FOO" );
-}, 3000 );
+		alert( "I have search of " );
+		alert( $("#search").val() );
+
+		chrome.devtools.network.getHAR( function( harLog ){
+			$("#debug").html( JSON.stringify( harLog ) );
+			alert( JSON.stringify( harLog ) );
+		} );
+	};
+
+	$("#debug").html( "Loading.." );
+
+	$("#go").click( function( ){
+		updatePanel( );
+	} );
+} );
